9a8763f0e1c1fd8d02bc7873c35f8fd0391388dc
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-19 06:49:51 +0200

    styles inline and  StyleSheet.create

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Hello! How are you!</Text>
+      <Text style={styles.text}> How are you!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    fontSize: 18,
+    
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+    fontSize: 36
+  }
 });
