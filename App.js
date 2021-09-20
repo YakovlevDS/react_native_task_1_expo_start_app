df6e4a66bdbdb53e7f3a0254b7977eaa2cb1a402
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-20 08:07:22 +0200

    added flatList keyExtractor

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import { Navbar } from './src/Navbar'
 import { AddTodo } from './src/AddTodo'
 import { Todo } from './src/Todo'
@@ -24,12 +24,14 @@ export default function App() {
       <Navbar title='Todo App!' />
       <View style={styles.container}>
         <AddTodo onSubmit={addTodo} />
+        <FlatList
+          keyExtractor={item=>item.id.toString()}
+          data={todos}
+          renderItem={({ item }) => (
+            <Todo todo={item}  />
+          )}/>
 
-        <View>
-          {todos.map(todo => (
-            <Todo todo={todo} key={todo.id} />
-          ))}
-        </View>
+       
       </View>
     </View>
   )
