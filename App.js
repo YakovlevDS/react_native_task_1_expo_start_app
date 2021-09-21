91bfa64c8200bfa21554131acc10c8abf4964365
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-21 08:45:56 +0200

    added window confirm

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Alert } from "react-native";
 import { Navbar } from "./src/components/Navbar";
 import { MainScreen } from "./src/screens/MainScreen";
 import { TodoScreen } from "./src/screens/TodoScreen";
@@ -22,9 +22,29 @@ export default function App() {
     ]);
   };
 
-  const removeTodo = id => {
-    setTodos(prev => prev.filter(todo => todo.id !== id));
+  const removeTodo = (id) => {
+    const todo = todos.find(t => t.id === id);
+    Alert.alert(
+      "Удаление элемента",
+      `Вы уверены, что хотите удалить "${todo.title}"?`,
+      [
+        {
+          text: "Отмена",
+          style: "cancel",
+        },
+        {
+          text: "Удалить",
+          style: "destructive",
+          onPress: () => {
+            setTodoId(null);
+            setTodos((prev) => prev.filter(todo => todo.id !== id));
+          },
+        },
+      ],
+      { cancelable: false }
+    );
   };
+
 
   let content = (
     <MainScreen
@@ -37,7 +57,13 @@ export default function App() {
 
   if (todoId) {
     const selectedTodo = todos.find(todo => todo.id === todoId);
-    content = <TodoScreen goBack={() => setTodoId(null)} todo={selectedTodo} />;
+    content = (
+      <TodoScreen
+        onRemove={removeTodo}
+        goBack={() => setTodoId(null)}
+        todo={selectedTodo}
+      />
+    );
   }
 
   return (
