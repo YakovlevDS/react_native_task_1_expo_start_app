7d175a99478b3631f218aa3e34dfedaa16253083
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-22 18:11:30 +0200

    added fonts

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useState } from "react";
+import * Font from 'expo-font'
 import { StyleSheet, Text, View, FlatList, Alert } from "react-native";
 import { Navbar } from "./src/components/Navbar";
 import { MainScreen } from "./src/screens/MainScreen";
 import { TodoScreen } from "./src/screens/TodoScreen";
+
+async function loadApplication() {
+  'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
+  'roboto-bold':require('./assets/fonts/Roboto-Bold.ttf'),
+}
 
 export default function App() {
   const [todoId, setTodoId] = useState(null);
