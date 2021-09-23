3d7d3d03a52029cf14cef17207aa2d47d976d373
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-23 16:23:42 +0200

    fix errors

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useState } from "react";
-import * Font from 'expo-font'
+import * as Font from 'expo-font'
 import { StyleSheet, Text, View, FlatList, Alert } from "react-native";
 import { Navbar } from "./src/components/Navbar";
 import { MainScreen } from "./src/screens/MainScreen";
 import { TodoScreen } from "./src/screens/TodoScreen";
 
 async function loadApplication() {
-  'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
-  'roboto-bold':require('./assets/fonts/Roboto-Bold.ttf'),
+  await Font.loadAsync({
+    "roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
 }
 
 export default function App() {
