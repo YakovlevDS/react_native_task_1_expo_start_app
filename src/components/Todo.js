31a54fcf28dfc291c166cc7b0000389ca3617456
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-23 18:25:55 +0200

    added useReduser

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { AppText } from '../components/ui/AppText'
 
 export const Todo = ({ todo, onRemove, onOpen }) => {
   return (
@@ -9,10 +10,10 @@ export const Todo = ({ todo, onRemove, onOpen }) => {
       onLongPress={onRemove.bind(null, todo.id)}
     >
       <View style={styles.todo}>
-        <Text>{todo.title}</Text>
+        <AppText>{todo.title}</AppText>
       </View>
     </TouchableOpacity>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
