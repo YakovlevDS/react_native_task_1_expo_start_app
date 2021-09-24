a90bf4ff3c9acb02047c70f230e52a6028803fe1
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-24 16:35:36 +0200

    add methods reducer

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -1,43 +1,61 @@
-import React, { useReducer, useContext } from 'react'
-import { Alert } from 'react-native'
-import { TodoContext } from './todoContext'
-import { todoReducer } from './todoReducer'
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types'
-import { ScreenContext } from '../screen/screenContext'
+import React, { useReducer, useContext } from "react";
+import { Alert } from "react-native";
+import { TodoContext } from "./todoContext";
+import { todoReducer } from "./todoReducer";
+import {
+  ADD_TODO,
+  REMOVE_TODO,
+  UPDATE_TODO,
+  SHOW_LOADER,
+  HIDE_LOADER,
+  SHOW_ERROR,
+  CLEAR_ERROR,
+} from "../types";
+import { ScreenContext } from "../screen/screenContext";
 
 export const TodoState = ({ children }) => {
   const initialState = {
-    todos: [{ id: '1', title: 'Выучить React Native' }]
-  }
-  const { changeScreen } = useContext(ScreenContext)
-  const [state, dispatch] = useReducer(todoReducer, initialState)
+    todos: [],
+    loading: false,
+    error: null,
+  };
+  const { changeScreen } = useContext(ScreenContext);
+  const [state, dispatch] = useReducer(todoReducer, initialState);
 
-  const addTodo = title => dispatch({ type: ADD_TODO, title })
+  const addTodo = (title) => dispatch({ type: ADD_TODO, title });
 
-  const removeTodo = id => {
-    const todo = state.todos.find(t => t.id === id)
+  const removeTodo = (id) => {
+    const todo = state.todos.find((t) => t.id === id);
     Alert.alert(
-      'Удаление элемента',
+      "Удаление элемента",
       `Вы уверены, что хотите удалить "${todo.title}"?`,
       [
         {
-          text: 'Отмена',
-          style: 'cancel'
+          text: "Отмена",
+          style: "cancel",
         },
         {
-          text: 'Удалить',
-          style: 'destructive',
+          text: "Удалить",
+          style: "destructive",
           onPress: () => {
-            changeScreen(null)
-            dispatch({ type: REMOVE_TODO, id })
-          }
-        }
+            changeScreen(null);
+            dispatch({ type: REMOVE_TODO, id });
+          },
+        },
       ],
       { cancelable: false }
-    )
-  }
+    );
+  };
 
-  const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title })
+  const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title });
+
+  const showLoader = () => dispatch({ type: SHOW_LOADER });
+
+  const hideLoader = () => dispatch({ type: HIDE_LOADER });
+
+  const showError = (error) => dispatch({ type: SHOW_ERROR, error });
+
+  const clearError = () => dispatch({ type: CLEAR_ERROR });
 
   return (
     <TodoContext.Provider
@@ -45,10 +63,10 @@ export const TodoState = ({ children }) => {
         todos: state.todos,
         addTodo,
         removeTodo,
-        updateTodo
+        updateTodo,
       }}
     >
       {children}
     </TodoContext.Provider>
-  )
-}
+  );
+};
--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -1,4 +1,13 @@
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types'
+import {
+  ADD_TODO,
+  REMOVE_TODO,
+  UPDATE_TODO,
+  SHOW_LOADER,
+  HIDE_LOADER,
+  SHOW_ERROR,
+  CLEAR_ERROR,
+  FETCH_TODOS,
+} from "../types";
 
 const handlers = {
   [ADD_TODO]: (state, { title }) => ({
@@ -7,27 +16,32 @@ const handlers = {
       ...state.todos,
       {
         id: Date.now().toString(),
-        title
-      }
-    ]
+        title,
+      },
+    ],
   }),
   [REMOVE_TODO]: (state, { id }) => ({
     ...state,
-    todos: state.todos.filter(todo => todo.id !== id)
+    todos: state.todos.filter((todo) => todo.id !== id),
   }),
   [UPDATE_TODO]: (state, { title, id }) => ({
     ...state,
-    todos: state.todos.map(todo => {
+    todos: state.todos.map((todo) => {
       if (todo.id === id) {
-        todo.title = title
+        todo.title = title;
       }
-      return todo
-    })
+      return todo;
+    }),
   }),
-  DEFAULT: state => state
-}
+  [SHOW_LOADER]: (state) => ({ ...state, loading: true }),
+  [HIDE_LOADER]: (state) => ({ ...state, loading: false }),
+  [CLEAR_ERROR]: (state) => ({ ...state, error: null }),
+  [SHOW_ERROR]: (state, { error }) => ({ ...state, error }),
+  [FETCH_TODOS]: (state, { todos }) => ({ ...state, todos }),
+  DEFAULT: (state) => state,
+};
 
 export const todoReducer = (state, action) => {
-  const handler = handlers[action.type] || handlers.DEFAULT
-  return handler(state, action)
-}
+  const handler = handlers[action.type] || handlers.DEFAULT;
+  return handler(state, action);
+};
